91326d5766cc940d1542252b995c136d0fa303d5
ArtARTs36 <temicska99@mail.ru>
2020-08-17 19:48:49 +0300

    link

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -224,7 +224,7 @@
                 <div class="offset-lg-3 col-lg-6 offset-md-2 col-md-8">
                     <div>
                         <h3>Остались вопросы? Свяжитесь с нами</h3>
-                        <a href="#0" class="butn light mt-30">
+                        <a href="/contacts" class="butn light mt-30">
                             <span>Задать вопрос<i class="pe-7s-paper-plane"></i></span>
                         </a>
                     </div>
