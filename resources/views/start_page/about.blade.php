46cca290c8f1106a7e66c3983415ea7006d77a95
ArtARTs36 <temicska99@mail.ru>
2020-08-18 03:14:18 +0300

    block about

---
+++ resources/views/start_page/about.blade.php
@@ -0,0 +1,31 @@
+<section class="banner section-padding" data-scroll-index="2">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-6 valign">
+                <div class="content mb-sm50" id="home-about-block">
+                    <h3>О нас.</h3>
+                    <p id="home-about-block__facts">
+                        <span>
+                            Фермерское хозяйство Украинского Виктора Николаевича  создано в 2002 году. Официальное название ИП ГКФХ Украинский В.Н
+                        </span>
+
+                        <span>
+                            Основным видом деятельности является пчеловодство. С каждым годом наращивается  количество обслуживаемых пчелосемей. В настоящее время пасека насчитывает 500 ульев, что позволяет собирать от 25 до 50 тонн меда в год.
+                        </span>
+                    </p>
+                    <a href="/about-us" class="butn butn-bg">
+                        <span>Читать полностью <i class="pe-7s-angle-right"></i></span>
+                    </a>
+                </div>
+            </div>
+
+            <div class="offset-md-1 col-md-5">
+                <div class="img">
+                    <img src="{{ asset('img/v3.svg') }}" alt="">
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
---
+++ resources/views/start_page/production_process.blade.php
@@ -0,0 +1,45 @@
+<section class="process">
+    <div class="section-padding background bg-img parallaxie" data-overlay-dark="9" data-background="img/bg1.jpg">
+        <div class="container">
+            <div class="row">
+
+                <div class="section-head text-center col-sm-12">
+                    <h6 class="sm-title">Взгляните на наш</h6>
+                    <h4>Процесс производства</h4>
+                </div>
+
+                <div class="col-lg-3 col-md-6 padding mb-md50">
+                    <div class="item text-center">
+                        <img src="{{ asset('img/arrow1.png') }}" class="tobotm" alt="">
+                        <span class="icon pe-7s-scissors"></span>
+                        <h6><span>01.</span>Отбор меда</h6>
+                        <p>Описание</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 padding mb-md50">
+                    <div class="item text-center">
+                        <img src="{{ asset('img/arrow1.png') }}" alt="">
+                        <span class="icon pe-7s-diamond"></span>
+                        <h6><span>02.</span>Откачка</h6>
+                        <p>Описание</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 padding mb-sm50">
+                    <div class="item text-center">
+                        <img src="{{ asset('img/arrow1.png') }}" class="tobotm" alt="">
+                        <span class="icon pe-7s-rocket"></span>
+                        <h6><span>03.</span>Фасовка</h6>
+                        <p>Описание</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 padding">
+                    <div class="item text-center">
+                        <span class="icon pe-7s-check"></span>
+                        <h6><span>04.</span>Результат</h6>
+                        <p>Описание</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
