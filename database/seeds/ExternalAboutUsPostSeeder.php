db49d1aa252cc5106ea64ca7fa7df71c71fe103d
ArtARTs36 <temicska99@mail.ru>
2020-08-18 04:10:44 +0300

    up external posts

--- database/seeds/ExternalAboutUsPostSeeder.php
+++ database/seeds/ExternalAboutUsPostSeeder.php
@@ -1,16 +1,29 @@
 <?php
 
 use App\Models\ExternalAboutUsPost;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class ExternalAboutUsPostSeeder extends Seeder
 {
     public function run(): void
     {
-        $count = 50;
+        ExternalAboutUsPost::query()->create([
+            ExternalAboutUsPost::FIELD_TITLE => 'Солнце в капле мёда золотого',
+            ExternalAboutUsPost::FIELD_LINK => 'https://communa.ru/obshchestvo/solntse_v_kaple_myeda_zolotogo_/',
+            ExternalAboutUsPost::FIELD_PUBLISHED_AT => Carbon::parse('13.08.2018 20:51'),
+        ]);
 
-        factory(ExternalAboutUsPost::class, $count)->create();
+        ExternalAboutUsPost::query()->create([
+            ExternalAboutUsPost::FIELD_TITLE => '«Цветы Черноземья» для школьников',
+            ExternalAboutUsPost::FIELD_LINK => 'http://adminknt.ru/2-uncategorised/2195-tsvety-chernozemya-dlya-shkolnikov.html',
+            ExternalAboutUsPost::FIELD_PUBLISHED_AT => Carbon::parse('26.12.2018 16:20'),
+        ]);
 
-        $this->command->comment("Create {$count} posts");
+        ExternalAboutUsPost::query()->create([
+            ExternalAboutUsPost::FIELD_TITLE => '«Школьный мёд» силы даёт',
+            ExternalAboutUsPost::FIELD_LINK => 'https://communa.ru/selskoe_khozyaystvo/shkolnyy-myed-sily-dayet/',
+            ExternalAboutUsPost::FIELD_PUBLISHED_AT => Carbon::parse('07.02.2019 18:52'),
+        ]);
     }
 }
