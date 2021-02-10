fa29c2d8597b5b2f2862d24a06846f9102af6645
aukrainskiy <aukrainskiy@phoenixit.ru>
2021-02-10 19:33:07 +0300

    update key words

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -4,9 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <meta name="keywords" content="HTML5 Template FULA onepage themeforest" />
-    <meta name="description" content="FULA - Onepage Multi-Purpose HTML5 Template" />
-    <meta name="author" content="" />
+    <meta name="keywords" content="Цветы, Черноземья, Воронеж, Мед, Пасека, Пчелы, Пчеловодство" />
+    <meta name="description" content="Цветы Черноземья" />
 
     <title>Цветы Черноземья</title>
 
